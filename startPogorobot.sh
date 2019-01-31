769b64e1f76585af8cdf35d8c4e136dae92f3208
Benjamin Marstaller <benjamin@marstaller.org>
2019-01-31 21:16:31 +0100

    Start first fat install set build chain

---
+++ startPogorobot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -z $1 ]
+then
+: # $1 was given
+VERSION=$1
+else
+: # $1 was not given
+VERSION='1.0.4'
+fi
+
+screen -S telegram-bot -d -m java -Dext.properties.dir=file:$PWD/config -XX:CompressedClassSpaceSize=190m -XX:MetaspaceSize=256M -Xmx600m -jar pogorobot-$VERSION.jar
