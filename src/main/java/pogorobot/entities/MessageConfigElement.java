3f569ed55c1960ce96aacf19f1aa2b2c39e71551
Benjamin Marstaller <benjamin@marstaller.org>
2019-03-10 22:22:09 +0100

    Refactor strings of config

--- src/main/java/pogorobot/entities/MessageConfigElement.java
+++ src/main/java/pogorobot/entities/MessageConfigElement.java
@@ -2,28 +2,50 @@ package pogorobot.entities;
 
 public enum MessageConfigElement {
 
+
 	// @formatter:off
-	CONFIG_ELEMENT_MONSTER("monster", null), 
-	CONFIG_ELEMENT_MONSTER_NOIV("monsterNoIv", null), 
-	CONFIG_ELEMENT_RAID("raid", null), 
-	CONFIG_ELEMENT_EGG("egg", null), 
-	CONFIG_ELEMENT_QUEST_SIMPLE("quest","simple"), 
-	CONFIG_ELEMENT_QUEST_SHORT("quest", "short"), 
-	CONFIG_ELEMENT_QUEST_GOOGLE_MONSTER("quest", "pokestop_google_monster"), 
-	CONFIG_ELEMENT_QUEST_APPLE_MONSTER("quest", "pokestop_apple_monster"), 
-	CONFIG_ELEMENT_QUEST_CANDY("quest", "pokestop_candy");
+	CONFIG_ELEMENT_MONSTER(Type.MONSTER, null), 
+	CONFIG_ELEMENT_MONSTER_NOIV(Type.MONSTER_NO_IV, null), 
+	CONFIG_ELEMENT_RAID(Type.RAID, null), 
+	CONFIG_ELEMENT_EGG(Type.EGG, null), 
+	CONFIG_ELEMENT_QUEST_SIMPLE(Type.QUEST,"simple"), 
+	CONFIG_ELEMENT_QUEST_SHORT(Type.QUEST, "short"), 
+	CONFIG_ELEMENT_QUEST_GOOGLE_MONSTER(Type.QUEST, "pokestop_google_monster"), 
+	CONFIG_ELEMENT_QUEST_APPLE_MONSTER(Type.QUEST, "pokestop_apple_monster"), 
+	CONFIG_ELEMENT_QUEST_CANDY(Type.QUEST, "pokestop_candy");
 	// @formatter:on
 
 	String key;
 	String subkey;
 
-	MessageConfigElement(String key, String subkey) {
-		this.key = key;
+	MessageConfigElement(Type type, String subkey) {
+		this.key = type.getType();
 		this.subkey = subkey;
 	}
 
 	public String getPath() {
 		String possibleSubelement = subkey != null && !subkey.trim().isEmpty() ? "." + subkey : "";
 		return key + possibleSubelement;
+	}
+
+	private enum Type {
+
+		// @formatter:off
+		MONSTER("monster"),
+		MONSTER_NO_IV("monsterNoIv"),
+		RAID("raid"),
+		EGG("egg"),
+		QUEST("quest");
+		// @formatter:on
+
+		private String type;
+
+		Type(String type) {
+			this.type = type;
+		}
+
+		public String getType() {
+			return type;
+		}
 	}
 }
